c1edec57677a576c6738d28bcff24579228d0290
eyesim.li <eyesim.li@vipshop.com>
2018-01-10 18:21:03 +0800

    lesson4

---
+++ node/lesson4/app.js
@@ -0,0 +1,62 @@
+// 这句的意思就是引入 `express` 模块，并将它赋予 `express` 这个变量等待使用。
+var express = require('express');
+var eventproxy = require('eventproxy');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var url = require('url');
+// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
+var app = express();
+var cnodeUrl = 'https://cnodejs.org/';
+// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
+// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
+// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
+app.get('/', function (req, sres, next) {
+    superagent.get(cnodeUrl)
+    .end(function (err, res) {
+        if (err) {
+            return console.error(err);
+        }
+        var topicUrls = [];
+        var $ = cheerio.load(res.text);
+        // 获取首页所有的链接
+        $('#topic_list .topic_title').each(function (idx, element) {
+            var $element = $(element);
+            // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
+            // 我们用 url.resolve 来自动推断出完整 url，变成
+            // https://cnodejs.org/topic/542acd7d5d28233425538b04 的形式
+            // 具体请看 http://nodejs.org/api/url.html#url_url_resolve_from_to 的示例
+            var href = url.resolve(cnodeUrl, $element.attr('href'));
+            topicUrls.push(href);
+        });
+        var ep = new eventproxy();
+        ep.after('topic_html', topicUrls.length, function(topics) {
+            topics = topics.map(function(topicPair) {
+                var topicUrl = topicPair[0];
+                var topicHtml = topicPair[1];
+                var $ = cheerio.load(topicHtml);
+                console.log($('.topic_full_title').text())
+                return ({
+                    title: $('.topic_full_title').text().trim(),
+                    href: topicUrl,
+                    comment1: $('.reply_content').eq(0).text().trim(),
+                });
+            })
+            // console.log('data:');
+            // console.log(topics)
+            sres.send(topics)
+        })
+        topicUrls.forEach(function(topicUrl) {
+            superagent.get(topicUrl)
+            .end(function(err, res) {
+                ep.emit('topic_html', [topicUrl, res.text]);
+            })
+            
+        })
+    });
+  });
+
+// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
+app.listen(8080, function () {
+  console.log('app is listening at port 8080');
+});
